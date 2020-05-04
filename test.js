2189dcd3075d984b970fb2ca592bba6a39144981
niket.vohra <niket.vohra@sofocle.com>
2020-05-04 16:22:13 +0530

    product testing scripts added

---
+++ test.js
@@ -0,0 +1,57 @@
+const rp = require('request-promise')
+const config = require('./flureeConfig.json')
+const schema = require('./resources/schema.json')
+const seedData = require('./resources/seed.json')
+//create a blank db
+async function newDb() {
+    let reqOptions = {
+        method: 'POST',
+        url: `${config.ip}/fdb/new-db`,
+        body: { 'db/id': `${config.network}/${config.db}` },
+        json: true
+    }
+    let db = await rp(reqOptions)
+    // console.log(db)
+}
+
+async function dbSchema() {
+    let reqOptions = {
+        method: 'POST',
+        url: `${config.ip}/fdb/${config.network}/${config.db}/transact`,
+        body: schema,
+        json: true
+    }
+    let sch = await rp(reqOptions)
+    // console.log(sch)
+}
+
+async function seed() {
+    let reqOptions = {
+        method: 'POST',
+        url: `${config.ip}/fdb/${config.network}/${config.db}/transact`,
+        body: seedData,
+        json: true
+    }
+    let s = await rp(reqOptions)
+    // console.log(s)
+}
+
+async function deploy() {
+    await newDb()
+    try {
+        await dbSchema()
+        if (seedData) {
+            await seed()
+        }
+    } catch (error) {
+        if (error.message.indexOf('The database does not exist within this ledger group') == -1) {
+            throw error
+        }
+        await dbSchema()
+        if (seedData) {
+            await seed()
+        }
+    }
+
+}
+deploy()
